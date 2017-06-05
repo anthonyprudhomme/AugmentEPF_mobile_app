ccbaac4a4e09d08025443bf38cbe2ef600567ca9
JimRaven <jmkduong@yahoo.fr>
2017-06-05 21:46:05 +0200

    Admin editing activities added

--- AugmentEPF/app/src/main/java/com/filiereticsa/arc/augmentepf/activities/AdminActivity.java
+++ AugmentEPF/app/src/main/java/com/filiereticsa/arc/augmentepf/activities/AdminActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.filiereticsa.arc.augmentepf.R;
 
@@ -15,25 +18,40 @@ public class AdminActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu (Menu menu){
-        getMenuInflater().inflate(R.menu.admin_menu, menu);
-        return super.onCreateOptionsMenu(menu);
+        getSupportActionBar().setTitle(R.string.admin_activity_title);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int itemID = item.getItemId();
-        Intent intent;
-        switch (itemID){
-            case android.R.id.home  :
+        if (item.getItemId()==android.R.id.home){
                 finish();
                 return true;
-
-            default: break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public void onFloor1Click(View view) {
+        Intent intent = new Intent(this, AdminEditActivity.class);
+        intent.putExtra("floor",1);
+        startActivity(intent);
+    }
+
+    public void onFloor2Click(View view) {
+        Intent intent = new Intent(this, AdminEditActivity.class);
+        intent.putExtra("floor",2);
+        startActivity(intent);
+    }
+
+    public void onFloor3Click(View view) {
+        Intent intent = new Intent(this, AdminEditActivity.class);
+        intent.putExtra("floor",3);
+        startActivity(intent);
+    }
+
+    public void onFloor4Click(View view) {
+        Intent intent = new Intent(this, AdminEditActivity.class);
+        intent.putExtra("floor",4);
+        startActivity(intent);
     }
 }
