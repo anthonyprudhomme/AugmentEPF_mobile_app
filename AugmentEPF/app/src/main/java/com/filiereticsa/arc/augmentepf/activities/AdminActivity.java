65931e43a58d534ff9931509b546abd1cab04038
Jeremiah Ravenor <jmkduong@yahoo.fr>
2017-06-05 16:13:48 +0200

    adminactivity added

--- AugmentEPF/app/src/main/java/com/filiereticsa/arc/augmentepf/activities/AdminActivity.java
+++ AugmentEPF/app/src/main/java/com/filiereticsa/arc/augmentepf/activities/AdminActivity.java
@@ -1,7 +1,10 @@
 package com.filiereticsa.arc.augmentepf.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.filiereticsa.arc.augmentepf.R;
 
@@ -11,5 +14,28 @@ public class AdminActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getActionBar().setLogo(R.drawable.logo_appli);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu){
+        getMenuInflater().inflate(R.menu.admin_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemID = item.getItemId();
+        Intent intent;
+        switch (itemID){
+            case android.R.id.home  :
+                finish();
+                return true;
+
+            default: break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
