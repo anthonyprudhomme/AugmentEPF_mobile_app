859fa9079f8e4a15417d9136ec4b710e79c1b54d
JimRaven <jmkduong@yahoo.fr>
2017-06-07 16:09:05 +0200

    First step towards server link

--- AugmentEPF/app/src/main/java/com/filiereticsa/arc/augmentepf/activities/AdminActivity.java
+++ AugmentEPF/app/src/main/java/com/filiereticsa/arc/augmentepf/activities/AdminActivity.java
@@ -1,14 +1,12 @@
 package com.filiereticsa.arc.augmentepf.activities;
 
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
@@ -16,37 +14,50 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.filiereticsa.arc.augmentepf.R;
+import com.filiereticsa.arc.augmentepf.interfaces.HTTPRequestInterface;
+import com.filiereticsa.arc.augmentepf.managers.HTTPRequestManager;
 
-public class AdminActivity extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class AdminActivity extends AppCompatActivity implements HTTPRequestInterface{
+    public static final String BEACONNAME = "beaconname";
+    public static final String BEACONXCOORD = "beaconxcoord";
+    public static final String BEACONYCOORD = "beaconycoord";
     private boolean editBeacon;
     private int current_floor;
     private ImageView iv;
+    private TextView closest;
+    private EditText xcoord, ycoord, name;
+    final private String ADMIN_MODIFICATION_PHP = "administrationChanges.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        editBeacon=true;
-        current_floor=0;
         setContentView(R.layout.activity_admin);
-        iv = (ImageView) findViewById(R.id.currentMap);
-        iv.setImageResource(R.drawable.floor_admin);
-        RadioButton rb = (RadioButton) findViewById(R.id.beaconEdit);
-        rb.setChecked(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(R.string.admin_activity_title);
+        iv = (ImageView) findViewById(R.id.currentMap);
+        RadioButton rb = (RadioButton) findViewById(R.id.beaconEdit);
+        closest = (TextView) findViewById(R.id.closestText);
+        xcoord = (EditText) findViewById(R.id.xCoordText);
+        ycoord = (EditText) findViewById(R.id.yCoordText);
+        iv.setImageResource(R.drawable.floor_admin);
+        rb.setChecked(true);
+        editBeacon = true;
+        current_floor = 0;
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.admin_menu,menu);
+        getMenuInflater().inflate(R.menu.admin_menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        //ImageView iv = (ImageView) findViewById(R.id.admin_imageView);
-        switch(item.getItemId()){
-            case android.R.id.home :
+        switch (item.getItemId()) {
+            case android.R.id.home:
                 finish();
                 break;
             case R.id.action_second_floor:
@@ -72,54 +83,43 @@ public class AdminActivity extends AppCompatActivity {
     }
 
     public void onBeaconClick(View view) {
-        TextView close = (TextView) findViewById(R.id.closestText);
-        close.setText("");
-        editBeacon=true;
+        closest.setText("");
+        editBeacon = true;
     }
 
     public void onPOIClick(View view) {
-        TextView close = (TextView) findViewById(R.id.closestText);
-        close.setText("");
-        editBeacon=false;
+        closest.setText("");
+        editBeacon = false;
     }
 
     public void onGPSClick(View view) {
-        EditText xcoord = (EditText) findViewById(R.id.xCoordText);
-        EditText ycoord = (EditText) findViewById(R.id.yCoordText);
-        TextView close = (TextView) findViewById(R.id.closestText);
         xcoord.setText("0.0");
         ycoord.setText("0.0");
-        if (editBeacon){
-            close.setText("B-747");
-        }else
-            close.setText("Cantina");
+        if (editBeacon) {
+            closest.setText("B-747");
+        } else
+            closest.setText("Cantina");
     }
 
     public void onSaveClick(View view) {
-        EditText name = (EditText) findViewById(R.id.item_name_Text);
+        if (name.getText().toString().matches("")) {
+            Toast.makeText(this, "Name required", Toast.LENGTH_SHORT).show();
+        } else
 
-        if (name.getText().toString().matches("")){
-            Toast.makeText(this,"Name required", Toast.LENGTH_SHORT).show();
-        }else
-
-        if (editBeacon){
-            Toast.makeText(this,name.getText() + " saved", Toast.LENGTH_SHORT).show();
-        }else Toast.makeText(this,name.getText() + " saved", Toast.LENGTH_SHORT).show();
+            save();
 
     }
 
     public void onRemoveClick(View view) {
-        final TextView close = (TextView) findViewById(R.id.closestText);
-
-        if (close.getText().toString().matches("")){
-            Toast.makeText(this,"None selected", Toast.LENGTH_SHORT).show();
-        }else {
+        if (closest.getText().toString().matches("")) {
+            Toast.makeText(this, "None selected", Toast.LENGTH_SHORT).show();
+        } else {
             DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
-                    switch (which){
+                    switch (which) {
                         case DialogInterface.BUTTON_POSITIVE:
-                            remove(close);
+                            remove();
                             break;
 
                         case DialogInterface.BUTTON_NEGATIVE:
@@ -132,14 +132,41 @@ public class AdminActivity extends AppCompatActivity {
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setMessage("Are you sure?").setPositiveButton("Yes", dialogClickListener)
                     .setNegativeButton("Cancel", dialogClickListener).show();
-
         }
     }
 
-    private void remove (TextView close){
-        if (editBeacon){
-            Toast.makeText(this, close.getText() + " removed", Toast.LENGTH_SHORT).show();
-        } else Toast.makeText(this,close.getText() + " removed", Toast.LENGTH_SHORT).show();
+    private void save() {
+        JSONObject jsonObject = new JSONObject();
+        String itemName = name.getText().toString();
+        String itemXCoord = xcoord.getText().toString();
+        String itemYCoord = ycoord.getText().toString();
+
+        if (editBeacon) {
+            try {
+                jsonObject.put(BEACONNAME, itemName);
+                jsonObject.put(BEACONXCOORD, itemXCoord);
+                jsonObject.put(BEACONYCOORD, itemYCoord);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            HTTPRequestManager.doPostRequest(ADMIN_MODIFICATION_PHP, jsonObject.toString(),
+                    this, HTTPRequestManager.BEACONS);
+
+            Toast.makeText(this, name.getText() + " saved", Toast.LENGTH_SHORT).show();
+        } else
+            Toast.makeText(this, name.getText() + " saved", Toast.LENGTH_SHORT).show();
     }
 
+    private void remove() {
+        if (editBeacon) {
+
+            Toast.makeText(this, closest.getText() + " removed", Toast.LENGTH_SHORT).show();
+        } else
+            Toast.makeText(this, closest.getText() + " removed", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRequestDone(String result) {
+
+    }
 }
